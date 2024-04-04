18804ba84f1f2d4c70444303a1ed69c9065e98dc
Ubuntu18 <304383736@qq.com>
2024-04-04 02:27:54 -0700

    ready to submit my lab-7

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,16 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  if(bstate.nthread == nthread){
+    bstate.round++;
+    bstate.nthread = 0;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  } else {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
@@ -63,6 +72,7 @@ main(int argc, char *argv[])
     exit(-1);
   }
   nthread = atoi(argv[1]);
+  
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
 
