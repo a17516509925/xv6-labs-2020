7e11ffff5e24639a9b4265d776e9d9db36d37d64
Ubuntu18 <304383736@qq.com>
2024-04-04 00:47:03 -0700

    ready to submit my lab-6

--- kernel/defs.h
+++ kernel/defs.h
@@ -64,6 +64,9 @@ void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
 
+int             krefcnt(void*);
+int             kaddrefcnt(void*);
+
 // log.c
 void            initlog(int, struct superblock*);
 void            log_write(struct buf*);
@@ -171,6 +174,9 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+
+int             cowpage(pagetable_t, uint64);
+void*           cowalloc(pagetable_t, uint64);
 
 // plic.c
 void            plicinit(void);
